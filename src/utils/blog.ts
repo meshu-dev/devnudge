bf57558b1b9a829ae40f1dba8304fca4952f641b
Mesh <harmeshuppal@gmail.com>
2024-04-17 20:14:16 +0100

    Integrate highlight.js library

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -2,8 +2,10 @@ import { getNotionPage, getNotionPages } from "./notion"
 import type { NotionPage } from "../types/notion"
 import type { BlogPage } from "../types/blog"
 import { getPageMarkdown } from "./notionMarkdown"
-import { marked } from "marked"
+import { Marked } from "marked"
 import type { MdStringObject } from "notion-to-md/build/types"
+import { markedHighlight } from "marked-highlight"
+import hljs from 'highlight.js'
 
 export const getPageId = async (slug: string): Promise<string|null> => {
   const pages = await getNotionPages()
@@ -19,6 +21,18 @@ export const getPageHeading = async (pageId: string): Promise<string|null> => {
 
 export const getPageBody = async (pageId: string): Promise<string|null> => {
   const markdown: MdStringObject = await getPageMarkdown(pageId)
+
+  const marked = new Marked(
+    markedHighlight({
+      langPrefix: 'hljs language-',
+      highlight(code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+        return hljs.highlight(code, { language }).value
+      }
+    })
+  )
+  
+
   return markdown.parent ? await marked.parse(markdown.parent) : null
 }
 
