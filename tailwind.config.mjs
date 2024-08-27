0ab03cb403e11cd0159cd142cfe25fb12a700f2e
Mesh <harmeshuppal@gmail.com>
2024-08-27 16:38:27 +0100

    Revert "Integrate flowbite and add index page"
    This reverts commit 4dcd5cba73a1596cf59ef94843ebdd046b082cb9.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		require('daisyui')
 	],
 }
